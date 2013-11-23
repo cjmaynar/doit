4dafdbef7ca7b08201076e9fca46a8766925e231
Corey Maynard <me@coreymaynard.com>
2013-11-23 16:05:36 -0500

    Add SQL file

---
+++ doit.sql
@@ -0,0 +1,17 @@
+CREATE TABLE `users` (
+`id` int(11) NOT NULL AUTO_INCREMENT,
+`username` varchar(45) NOT NULL,
+`password` varchar(256) NOT NULL,
+PRIMARY KEY (`id`),
+UNIQUE KEY `username_UNIQUE` (`username`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1 
+
+CREATE TABLE `tasks` (
+`id` int(11) NOT NULL AUTO_INCREMENT,
+`user` int(11) NOT NULL,
+`task` varchar(255) NOT NULL,
+`created` datetime NOT NULL,
+`completed` datetime DEFAULT NULL,
+`due` date DEFAULT NULL,
+PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1
